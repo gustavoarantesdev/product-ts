fb47a2fe0bc4fe4e0466316f81edaa693d3abe1a
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-05-22 16:43:59 -0300

    feat: adiciona uso do middleware de rota não encontrada

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import routes from "./routes/index.js";
+import notFound from "./middleware/not-found.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
+
+app.use(notFound);
 
 export default app;
