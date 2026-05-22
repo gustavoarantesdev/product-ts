953cc726e9f55de915b3896cb364a0749abab75d
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-05-22 16:31:24 -0300

    feat: adiciona função para retornar produtos

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,2 +1,6 @@
 import type { Request, Response } from "express";
 import type { Product } from "../types/product.js";
+
+export function getProducts(req: Request, res: Response) {
+  res.status(200).send("Todos os produtos");
+}
