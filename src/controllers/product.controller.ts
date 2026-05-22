f5d7c894d91db56f658ba1e640463607b35c4f7c
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-05-22 16:48:03 -0300

    feat: adiciona funções base

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -4,3 +4,19 @@ import type { Product } from "../types/product.js";
 export function getProducts(req: Request, res: Response) {
   res.status(200).send("Todos os produtos");
 }
+
+export function getProductById(req: Request, res: Response) {
+  //
+}
+
+export function createProduct(req: Request, res: Response) {
+  //
+}
+
+export function updateProduct(req: Request, res: Response) {
+  //
+}
+
+export function deleteProduct(req: Request, res: Response) {
+  //
+}
