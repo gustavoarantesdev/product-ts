87475a19a7088572fbcfa58406b3fbb8a5965b52
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-05-23 11:54:16 -0300

    feat: adiciona função para cadastrar produto

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,12 +1,10 @@
-import type { Request, Response } from "express";
+import { json, type Request, type Response } from "express";
 import * as productService from "../services/product.service.js";
+import type { Product } from "../types/product.js";
 
-export async function getAllProducts(
-  req: Request,
-  res: Response,
-): Promise<Response> {
+export async function getAll(req: Request, res: Response): Promise<Response> {
   try {
-    const products = await productService.listAllProducts();
+    const products = await productService.getAll();
 
     return res.status(200).json(products);
   } catch (error: any) {
@@ -16,12 +14,20 @@ export async function getAllProducts(
   }
 }
 
-export function getProductById(req: Request, res: Response) {
+export function getById(req: Request, res: Response) {
   //
 }
 
-export function createProduct(req: Request, res: Response) {
-  //
+export async function create(req: Request, res: Response): Promise<Response> {
+  const product: Product = req.body;
+
+  try {
+    await productService.create(product);
+
+    return res.status(201).json({ message: "Product created" });
+  } catch (error: any) {
+    return res.status(400).json({ error: error.message });
+  }
 }
 
 export function updateProduct(req: Request, res: Response) {
