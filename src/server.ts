df8a0d0809febc7e139fc647b8ac57b5da45c690
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-05-22 23:49:35 -0300

    feat: adiciona função bootstrap

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
 import app from "./app.js";
+import { initDatabase } from "./database/init.js";
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+async function bootstrap() {
+  try {
+    await initDatabase();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(`Failed to star application: ${error}`);
+    process.exit(1);
+  }
+}
+
+bootstrap();
