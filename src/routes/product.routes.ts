18d2bf17cb5675cab587aba037619cd125c04a28
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-05-23 11:53:03 -0300

    feat: adiciona rota para cadastrar produto

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -3,6 +3,8 @@ import * as productController from "../controllers/product.controller.js";
 
 const productRoutes = Router();
 
-productRoutes.get("/", productController.getAllProducts);
+productRoutes.get("/", productController.getAll);
+
+productRoutes.post("/create", productController.create);
 
 export default productRoutes;
