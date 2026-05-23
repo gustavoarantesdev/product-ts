313efa079185812a77cfec9ec08eec1bb2cbd124
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-05-23 11:54:35 -0300

    feat: adiciona função para cadastrar produto

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,5 +1,10 @@
-import { findAllProducts } from "../repositories/product.repository.js";
+import * as productRepository from "../repositories/product.repository.js";
+import type { Product } from "../types/product.js";
 
-export async function listAllProducts() {
-  return await findAllProducts();
+export async function getAll() {
+  return await productRepository.getAll();
+}
+
+export async function create(product: Product): Promise<void> {
+  return await productRepository.create(product);
 }
