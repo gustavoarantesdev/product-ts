dfe68d2d04a0a673af9d9f3df79890c88d31df89
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-05-23 11:54:58 -0300

    feat: adiciona função para salvar produto

--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -3,10 +3,22 @@ import type { Product } from "../types/product.js";
 
 const db = await getConnection();
 
-export async function findAllProducts(): Promise<Product[]> {
+export async function getAll(): Promise<Product[]> {
   const rows = await db.all(`
     SELECT id, name, description, status FROM products
     `);
 
   return rows as Product[];
+}
+
+export async function create(product: Product): Promise<void> {
+  const { name, description } = product;
+
+  const stmt = await db.prepare(
+    "INSERT INTO products (name, description) VALUES (?, ?)",
+  );
+
+  await stmt.run([name, description]);
+
+  await stmt.finalize();
 }
