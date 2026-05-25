0f606af6a8a501a8997c692848424cbf2a0a1139
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-05-25 00:21:11 -0300

    feat: adiciona função para validar a localização de produto por id

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,8 +1,20 @@
 import * as productRepository from "../repositories/product.repository.js";
 import type { Product } from "../types/product.js";
 
-export async function getAll() {
+export async function getAll(): Promise<Product[] | null> {
   return await productRepository.getAll();
+}
+
+export async function getById(id: number): Promise<Product | string> {
+  const product = await productRepository.getById(id);
+
+  console.log(product);
+
+  if (!product) {
+    return "Product not found";
+  }
+
+  return product;
 }
 
 export async function create(product: Product): Promise<void> {
