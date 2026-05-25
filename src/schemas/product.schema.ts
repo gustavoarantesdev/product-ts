63d7e13607e5f0cb13fedfa8221968607eed852b
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-05-25 17:10:29 -0300

    feat: adiciona schema para produtos com zod

---
+++ src/schemas/product.schema.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export const productSchema = z.object({
+  id: z.number(),
+  name: z.string().min(3),
+  description: z.string(),
+  status: z.number(),
+  updated_at: z.date(),
+  created_at: z.date(),
+});
+
+export type Product = z.infer<typeof productSchema>;
+
+export const createProductSchema = z.object({
+  name: z.string().min(3),
+  description: z.string(),
+});
+
+export type CreateProductDTO = z.infer<typeof createProductSchema>;
+
+export const updateProductSchema = z.object({
+  name: z.string().min(3),
+  description: z.string(),
+  status: z.number().optional(),
+});
+
+export type UpdateProductDTO = z.infer<typeof updateProductSchema>;
