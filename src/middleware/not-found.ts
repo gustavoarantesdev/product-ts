d5fdf3ac0f1e6dd7b02e65d0856df422cbe5c314
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-05-28 16:31:49 -0300

    feat: adiciona proprieade next

--- src/middleware/not-found.ts
+++ src/middleware/not-found.ts
@@ -1,6 +1,10 @@
-import type { Request, Response } from "express";
+import type { Request, Response, NextFunction } from "express";
 
-export default function notFound(req: Request, res: Response): void {
+export default function notFound(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
   res.status(404).json({
     message: "Route not found",
   });
